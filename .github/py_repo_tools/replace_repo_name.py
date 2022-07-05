bc6158a2e5f9346a8ae69c68203c89dbaaf335d2
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-04 20:26:26 -0400

    Update replace_repo_name.py

--- .github/py_repo_tools/replace_repo_name.py
+++ .github/py_repo_tools/replace_repo_name.py
@@ -4,10 +4,7 @@ FileName = "README.md"
 with open(FileName, 'r') as f:
     contents = f.read()
     # Define the first line where your content will be replaced / added 
-    contents = contents.replace('fork-me.min.js', 'media-element.min.js')
-    contents = contents.replace('fork-me.js', 'media-element.js')
-    contents = contents.replace('Fork-Me.js', 'Media-Element.js')
-    
+    contents = contents.replace('Media-Element.js/media-element.min.js', 'version/1.0.1/src/media-elements.js')
     
 with open(FileName, 'w') as f:
     f.write(contents)   
