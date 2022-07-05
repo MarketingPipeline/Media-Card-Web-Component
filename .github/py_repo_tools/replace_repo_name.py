04879afb2ef8ff4602cbad6c365f203b8048d7e4
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-04 20:18:13 -0400

    Update replace_repo_name.py

--- .github/py_repo_tools/replace_repo_name.py
+++ .github/py_repo_tools/replace_repo_name.py
@@ -4,7 +4,10 @@ FileName = "README.md"
 with open(FileName, 'r') as f:
     contents = f.read()
     # Define the first line where your content will be replaced / added 
-    contents = contents.replace('MarketingPipeline/Fork-Me', 'MarketingPipeline/Media-Element')
+    contents = contents.replace('fork-me.min.js', 'media-element.min.js')
+    contents = contents.replace('fork-me.js', 'media-element.js')
+    contents = contents.replace('Fork-Me.js', 'Media-Element.js')
+    
     
 with open(FileName, 'w') as f:
     f.write(contents)   
